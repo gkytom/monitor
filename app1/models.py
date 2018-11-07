3924faee7bb743e51c40dfc94814d3ea46e66a8c
gkytom <448519266@qq.com>
2018-11-07 19:00:05 +0800

    20181107_01

--- app1/models.py
+++ app1/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class System(models.Model):
+    sys_name = models.CharField(max_length=100)
+
 class Machine(models.Model):
-    name = models.CharField(max_length=100)
+    hostname = models.CharField(max_length=100)
+    procname = models.CharField(max_length=100)
+    listen_port=models.IntegerField
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+    system = models.ForeignKey(System,on_delete=models.SET_NULL)
+
+
